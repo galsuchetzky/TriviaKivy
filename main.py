1e5957d8f0bf134942ff7bf4872f3d9ffc85781f
Gal Suchetzky <galsuchetzky@gmail.com>
2022-09-26 11:15:40 +0300

    added game modes

--- main.py
+++ main.py
@@ -99,6 +99,7 @@ class GameModeSelectionWindow(Screen):
         manager.current = 'main'
 
     def start_game(self, mode):
+        manager.get_screen('game').mode = mode
         manager.current = 'load_screen'
 
 
@@ -119,12 +120,29 @@ class GameWindow(Screen):
         self.num_questions = 0
         self.correct_answers = 0
         self.timer = None
+        self.mode = 0
+        self.max_mistakes = None
+        self.current_mistakes = 0
 
     def on_pre_enter(self, *args):
         self.num_questions = len(self.questions)
         self.correct_answers = 0
+        self.current_mistakes = 0
         self.ids.score.text = str(self.correct_answers) + '/' + str(self.num_questions)
-        self.timer = Timer(self.ids.timer)  # set depending on game mode
+        if self.mode == 0:
+            self.timer = Timer(self.ids.timer)
+            self.max_mistakes = None
+        elif self.mode == 1:
+            self.timer = Timer(self.ids.timer)
+            self.max_mistakes = 3
+        elif self.mode == 2:
+            self.timer = Timer(self.ids.timer)
+            self.max_mistakes = 1
+        elif self.mode == 3:
+            self.timer = Timer(self.ids.timer,
+                               countdown=True,
+                               countdown_start_time=180,
+                               countdown_callback=self.finish_game)
         self.ids.timer.text = str(self.timer)
         self.timer.start()
 
@@ -132,10 +150,7 @@ class GameWindow(Screen):
         self.set_question()
 
     def set_question(self, *args):
-        if not self.questions:
-            manager.get_screen('game_score').final_score = str(self.correct_answers) + '/' + str(self.num_questions)
-            manager.current = 'game_score'
-            self.timer.stop()
+        if self.is_game_finished():
             return
 
         self.current_question = self.questions.pop()
@@ -160,10 +175,24 @@ class GameWindow(Screen):
         else:
             self.ids['ans' + str(selected_ans)].background_normal = ''
             self.ids['ans' + str(selected_ans)].background_color = 1, 0, 0, 1
+            self.current_mistakes += 1
 
         self.ids.score.text = str(self.correct_answers) + '/' + str(self.num_questions)
         Clock.schedule_once(self.set_question, 1)
 
+    def is_game_finished(self):
+        print(self.current_mistakes, self.max_mistakes)
+        if not self.questions or\
+                (self.max_mistakes is not None and self.current_mistakes == self.max_mistakes):
+            self.finish_game()
+            return True
+        return False
+
+
+    def finish_game(self):
+        manager.get_screen('game_score').final_score = str(self.correct_answers) + '/' + str(self.num_questions)
+        manager.current = 'game_score'
+        self.timer.stop()
 
 class LoadingWindow(Screen):
     def __init__(self, name):
